e70965d17d8cff6afce04e9be104e31cae1049c0
venkataranga <dinesh.csit@gmail.com>
2015-05-20 18:31:28 -0500

    other inner class types

--- JavaTraining/src/com/javatraining/innerclasses/Main.java
+++ JavaTraining/src/com/javatraining/innerclasses/Main.java
@@ -5,9 +5,20 @@ import com.javatraining.innerclasses.OuterClass.InnerClass;
 public class Main {
 
 	public static void main(String[] args) {
-		OuterClass outer = new OuterClass();
-		OuterClass.InnerClass inner = outer.new InnerClass();
+		
+		OuterClass outer = new OuterClass(5);
+		//InnerClass innerr = new InnerClass();
+		OuterClass.InnerClass inner = new OuterClass(5).new InnerClass();
+		OuterClass.InnerClass inner2 = new OuterClass(10).new InnerClass();
 		inner.print();
+		inner2.print();
+		
+		//inner.printOuterI(OuterClass.);
+		
+		OuterClass o = new OuterClass(1000000);
+		o.createOuterClassInstance();
+		
+		o.methodLocalInnerClass(0);
 	}	
 	
 }
--- JavaTraining/src/com/javatraining/innerclasses/OuterClass.java
+++ JavaTraining/src/com/javatraining/innerclasses/OuterClass.java
@@ -2,13 +2,56 @@ package com.javatraining.innerclasses;
 
 public class OuterClass {
 
-	private int i =10;
-	
+	private int i;
 	class InnerClass {
 		public void print() {
-			System.out.println(i);
-
+			
+			System.out.println(this.getClass().getName());
+			System.out.println(OuterClass.this);
+			printOuterI(OuterClass.this);
+			
+		}
+		public void printOuterI(OuterClass outter){
+			System.out.println(outter.i);
 		}
 	}
+	
+	
+	public OuterClass(int i){
+		this.i = i;
+		
+	}
+	
+	public void createOuterClassInstance(){
+		//InnerClass in = new InnerClass();
+		
+	}
+	
+	public void methodLocalInnerClass(final int j){
+		
+		
+		class MethodLocal {
+			int local=0;
+			public  void doIHaveAccessToPrivateVraibles(){
+				System.out.println(i);
+				System.out.println(j);
+			}
+		}
+		
+		
+		MethodLocal ml = new MethodLocal();
+		ml.doIHaveAccessToPrivateVraibles();
+		
+		class MethodLocal2{
+			public void some(){
+				MethodLocal l = new MethodLocal();
+				System.out.println(l.local);
+			}
+		}
+	}
+	
+	 
+	
+	
 	
 }
