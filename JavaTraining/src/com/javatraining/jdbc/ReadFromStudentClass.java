06860b29bbde200f99a0c7e73c62ed50ffc1d95f
venkataranga <dinesh.csit@gmail.com>
2015-05-21 17:41:31 -0500

    jdbc read example

---
+++ JavaTraining/src/com/javatraining/jdbc/ReadFromStudentClass.java
@@ -0,0 +1,54 @@
+package com.javatraining.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class ReadFromStudentClass {
+
+	public static void main(String[] args) throws SQLException {
+
+		final String QUERY = "select * from student";
+
+		
+
+		ResultSet result = getResult(QUERY);
+
+		while (result.next()) {
+			System.out.println("___________________");
+			System.out.print(result.getInt(1)+" ");
+			System.out.print(result.getString(2)+" ");
+			System.out.print(result.getString(3)+" ");
+			System.out.print(result.getString(4)+" ");
+			System.out.print(result.getString(5)+" ");
+			System.out.print(result.getString(6)+" ");
+			System.out.print(result.getInt("age")+"\n");
+		}
+
+	}
+
+	public static ResultSet getResult(String sqlQuery) {
+
+		Connection connection = null;
+		ResultSet result = null;
+		try {
+
+			// Class.forName("com.mysql.jdbc.Driver");
+
+			connection = DriverManager.getConnection(
+					"jdbc:mysql://localhost:3306/JDBCTraining", "root",
+					"dinesh");
+
+			Statement statement = connection.createStatement();
+
+			result = statement.executeQuery(sqlQuery);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+}
